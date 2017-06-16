4fe0a4051252c92af6f3c6f0d2a39d967a040668
PMiskew <paul.miskew@gmail.com>
2017-06-15 22:10:24 -0400

    First Commit

---
+++ TurtleTriangle.py
@@ -0,0 +1,30 @@
+import turtle
+
+
+bob = turtle.Turtle()
+
+
+angle1 = 60
+angle2 = 60
+angle3 = 180 - angle1 - angle2
+
+side = 100
+
+bob.forward(side)
+bob.right(angle1)
+
+
+bob.forward(side)
+bob.right(angle1)
+
+
+
+bob.forward(side)
+bob.right(angle1)
+
+
+
+
+
+
+turtle.done()
